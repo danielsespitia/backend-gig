ef12715f5061ae082f2e69e63d2896b09a67ccfb
danielsespitia <63252057+danielsespitia@users.noreply.github.com>
2021-02-08 21:49:54 -0500

    created profile picture controller, endpoint,  cloudinaryPresets and busboy middleware (#8)

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 const { auth } = require("../utils/auth");
+const userProfilePicture = require("../utils/cloudinaryPresets/userProfilePicture");
 
 router.route("/sign-up").post(userController.signup);
 router.route("/sign-in").post(userController.signin);
 router.route("/").get(userController.list);
 router.route("/profile").get(auth, userController.show);
 router.route("/").put(auth, userController.update);
+router
+  .route("/update-pp")
+  .put(auth, userProfilePicture, userController.updateProfilePicture);
 router.route("/").delete(auth, userController.destroy);
 
 module.exports = router;
